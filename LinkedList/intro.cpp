0830701f8815cd67c8c95a30bd1232ddaac06606
Nishant <chaudhary2306@gmail.com>
2021-12-20 17:14:52 +0530

    enter

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -7,3 +7,51 @@
 // Memory efficiency: For the same number of elements, linked lists use more memory as a reference to the next node is also stored along with the data. However, size flexibility in linked lists may make them use less memory overall; this is useful when there is uncertainty about size or there are large variations in the size of data elements; memory equivalent to the upper limit on the size has to be allocated (even if not all of it is being used) while using arrays, whereas linked lists can increase their sizes step-by-step proportionately to the amount of data.
 
 // Execution time: Any element in an array can be directly accessed with its index; however in the case of a linked list, all the previous elements must be traversed to reach any element. Also, better cache locality in arrays (due to contiguous memory allocation) can significantly improve performance. As a result, some operations (such as modifying a certain element) are faster in arrays, while some others (such as inserting/deleting an element in the data) are faster in linked lists.
+
+// A simple C++ program for traversal of a linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* next;
+};
+
+// This function prints contents of linked list
+// starting from the given node
+void printList(Node* n)
+{
+	while (n != NULL) {
+		cout << n->data << " ";
+		n = n->next;
+	}
+}
+
+// Driver code
+int main()
+{
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	// allocate 3 nodes in the heap
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	head->data = 1; // assign data in first node
+	head->next = second; // Link first node with second
+
+	second->data = 2; // assign data to second node
+	second->next = third;
+
+	third->data = 3; // assign data to third node
+	third->next = NULL;
+
+	printList(head);
+
+	return 0;
+}
+
+// This is code is contributed by rathbhupendra
