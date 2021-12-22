716a187ba3851d9c9dc914e65e701b3fe6abc7ca
Nishant <chaudhary2306@gmail.com>
2021-12-22 20:01:24 +0530

    enter

--- LinkedList/insertion.cpp
+++ LinkedList/insertion.cpp
@@ -20,6 +20,14 @@ Node* InsertAtFront(Node* head, int new_data){
     new_node -> next = head;
     return new_node;
 }
+
+Node* InsertInMid(Node* head, int new_data){
+    Node* new_node = new Node;
+    new_node->data = new_data;
+    new_node->next = head->next;
+    head-> next = new_node;
+    return new_node;
+}
 int main(){
     Node* head = new Node();
     Node* ptr = head;
@@ -30,9 +38,14 @@ int main(){
         ptr = ptr-> next;
     }
     cin >> ptr-> data;
-    cout << "Enter the integer you want to insert:- \n";
-    int a;
-    cin >> a;
-    head = InsertAtFront(head, a);
+    // cout << "Enter the integer you want to insert in starting:- \n";
+    // int a;
+    // cin >> a;
+    // head = InsertAtFront(head, a);
+    cout << "Enter the integer you want to insert in mid:- \n";
+    int b;
+    cin >> b;
+    cout << "enter the place at which you want to enter the element:- \n";
+    head = InsertInMid(head, b);
     printList(head);
 }
---
+++ LinkedList/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ant to insert in starting:- \n";
+    // int a;
+    // cin >> a;
+    // head = InsertAtFront(head, a);
