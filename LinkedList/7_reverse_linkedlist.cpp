efcdc4b9b5b088a4d8335f4a0cf9e79099832500
Nishant <chaudhary2306@gmail.com>
2022-01-14 20:23:14 +0530

    enter

---
+++ LinkedList/7_reverse_linkedlist.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void printList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+Node *reverseITR(Node *head)
+{
+    Node *next = NULL;
+    Node *current = head;
+    Node *prev = NULL;
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
+}
+int main()
+{
+    Node *head = new Node();
+    Node *ptr = head;
+    int n = 5;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> ptr->data;
+        ptr->next = new Node();
+        ptr = ptr->next;
+    }
+    cin >> ptr->data;
+    head = reverseITR(head);
+    printList(head);
+    return 0;
+}
