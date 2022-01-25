ec978e08bf1e0f43ad29f63f37faec04f1c42e6a
Nishant <chaudhary2306@gmail.com>
2022-01-25 20:24:22 +0530

    enter

---
+++ STL/2.List/functions.cpp
@@ -0,0 +1,62 @@
+/*
+1.front()-Returns the value of the first element in the list.
+
+2.back()-Returns the value of the last element in the list.
+
+3.push_front(g)-Adds a new element ‘g’ at the beginning of the list.
+
+4.push_back(g) -Adds a new element ‘g’ at the end of the list.
+
+5.pop_front()-Removes the first element of the list, and reduces size of the list by 1.
+
+6.pop_back()-Removes the last element of the list, and reduces size of the list by 1.
+
+7.list::begin()-begin() function returns an iterator pointing to the first element of the list.
+
+8.list::end()-end() function returns an iterator pointing to the theoretical last element which follows the last element.
+
+9.list rbegin() and rend()-rbegin() returns a reverse iterator which points to the last element of the list. rend() returns a reverse iterator which points to the position before the beginning of the list.
+
+10.list cbegin() and cend() - cbegin() returns a constant random access iterator which points to the beginning of the list. cend() returns a constant random access iterator which points to the end of the list.
+
+11.list crbegin() and crend() -crbegin() returns a constant reverse iterator which points to the last element of the list i.e reversed beginning of container. crend() returns a constant reverse iterator which points to the theoretical element preceding the first element in the list i.e. the reverse end of the list.
+
+12.empty() -Returns whether the list is empty(1) or not(0).
+
+13.insert()-Inserts new elements in the list before the element at a specified position.
+
+14.erase()-Removes a single element or a range of elements from the list.
+
+15.assign()-Assigns new elements to list by replacing current elements and resizes the list.
+
+16.remove()-Removes all the elements from the list, which are equal to given element.
+
+17.list::remove_if()-Used to remove all the values from the list that correspond true to the predicate or condition given as parameter to the function.
+
+18.reverse()-Reverses the list.
+
+19.size()-Returns the number of elements in the list.
+
+20.list resize()-Used to resize a list container.
+
+21.sort()-Sorts the list in increasing order.
+
+22.list max_size() -Returns the maximum number of elements a list container can hold.
+
+23.list unique()-Removes all duplicate consecutive elements from the list.
+
+24.list::emplace_front() and list::emplace_back()-emplace_front() function is used to insert a new element into the list container, the new element is added to the beginning of the list. emplace_back() function is used to insert a new element into the list container, the new element is added to the end of the list.
+
+25.list::clear()-clear() function is used to remove all the elements of the list container, thus making it size 0.
+
+26.list::operator=-This operator is used to assign new contents to the container by replacing the existing contents.
+
+27.list::swap()-This function is used to swap the contents of one list with another list of same type and size.
+
+28.list splice()-Used to transfer elements from one list to another.
+
+29.list merge()-Merges two sorted lists into one.
+
+30.list emplace()-Extends list by inserting new element at a given position.
+
+*/
