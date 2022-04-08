bc3cd84254d40add28bed70e4fa349048ebb7ff3
Nishant2306 <nishantc20@cse.iiitp.ac.in>
2022-04-08 11:47:17 +0530

    Enter

--- LinkedList/10_detect_and_remove_loop.cpp
+++ LinkedList/10_detect_and_remove_loop.cpp
@@ -16,38 +16,53 @@ void printList(Node *head)
     }
     cout << endl;
 }
-void DetectAndRemove(Node *head){
-    Node *slow = head;
-    Node *fast = head;
-    while(slow!=NULL and fast!=NULL){
-        slow = slow->next;
-        fast = fast->next->next;
-        if(slow==fast){
-            cout << "YES\n";
+// void DetectAndRemove1(Node *head){
+//     Node *slow = head;
+//     Node *fast = head;
+//     while(slow!=NULL and fast!=NULL){
+//         slow = slow->next;
+//         fast = fast->next->next;
+//         if(slow==fast){
+//             cout << "YES\n";
+//             break;
+//         }
+//     }
+//     Node *ptr1 = slow;
+//     Node *ptr2 = slow;
+//     int k = 1;
+//     cout << ptr1->data << endl;
+//     while(ptr1->next != ptr2){
+//         ptr1 = ptr1->next;
+//         k++;
+//     }
+//     ptr1 = head;
+//     ptr2 = head;
+//     for(int i = 0; i<k; i++){
+//         ptr2 = ptr2->next;
+//     }
+//     while(ptr2!=ptr1){
+//         ptr1 = ptr1->next;
+//         ptr2 = ptr2->next;
+//     }
+//     while(ptr2->next != ptr1){
+//         ptr2 = ptr2->next;
+//     }
+//     ptr2->next = NULL;
+// }
+
+void DetectAndRemove2(Node *head){
+    unordered_map<Node*, int> n;
+    Node *last = NULL;
+    while(head != NULL){
+        if (n.find(head) == n.end()) {
+            n[head]++;
+            last = head;
+            head = head->next;
+        }else {
+            last->next = NULL;
             break;
         }
-    }
-    Node *ptr1 = slow;
-    Node *ptr2 = slow;
-    int k = 1;
-    cout << ptr1->data << endl;
-    while(ptr1->next != ptr2){
-        ptr1 = ptr1->next;
-        k++;
-    }
-    ptr1 = head;
-    ptr2 = head;
-    for(int i = 0; i<k; i++){
-        ptr2 = ptr2->next;
-    }
-    while(ptr2!=ptr1){
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next;
-    }
-    while(ptr2->next != ptr1){
-        ptr2 = ptr2->next;
-    }
-    ptr2->next = NULL;
+    } 
 }
 int main()
 {
@@ -62,7 +77,8 @@ int main()
     }
     cin >> ptr->data;
     ptr->next = head->next;
-    DetectAndRemove(head);
+    // DetectAndRemove1(head);
+    DetectAndRemove2(head);
     printList(head);
     return 0;
 }
